fd1ec0ed7bb3575dfbeed610c9e1788e12f02e83
kvchi <jonathanmkpuma@gmail.com>
2025-07-08 15:54:05 +0100

    js adjustment and css adjustment

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 
 const input = document.getElementById("display");
-const buttons = document.querySelectorAll(".btn");
+const buttons = document.querySelectorAll(".btn, .symbolsBtn");
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
-        const value = button.textContent;
+        const value = button.textContent.trim();
         if(value === "=") {
             try {
                 input.value = eval(input.value)
